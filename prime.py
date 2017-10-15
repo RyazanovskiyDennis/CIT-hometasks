8cec32d357d6afd55d6dbc863ce816a812ac3f5c
RyazanovskiyDennis <RyazanovskiyDennis@gmail.com>
2017-10-15 20:18:46 +0300

    commented logs

--- prime.py
+++ prime.py
@@ -3,10 +3,10 @@ import math
 n = int(input("Вывод простых чисел до числа (включительно): "))
 a = [True] * n
 for i in range(2,math.ceil(math.sqrt(n))+1):
-    print('i = ', i)
+    #print('i = ', i)
     if a[i-1]:
         for j in range(i * 2, n+1, i):
-            print('\tj = ', j)
+            #print('\tj = ', j)
             a[j-1] = False
 b = [i for i in range(2, n+1) if a[i-1]]
 print(b)
